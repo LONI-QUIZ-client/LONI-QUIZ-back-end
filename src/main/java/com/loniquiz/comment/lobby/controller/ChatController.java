00c68c27a673dc83561ecb9c9decd5d721497f69
SeungHanLee <swq3064@naver.com>
2024-01-26 16:52:16 +0900

    # 웹소켓 채팅 일단 구현

---
+++ src/main/java/com/loniquiz/comment/lobby/controller/ChatController.java
@@ -0,0 +1,20 @@
+package com.loniquiz.comment.lobby.controller;
+
+import com.loniquiz.chatEntity.ChatResponse;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import java.util.Date;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public ChatResponse sendMessage(@Payload ChatResponse res) {
+        res.setTimestamp(new Date());
+        return res;
+    }
+}
