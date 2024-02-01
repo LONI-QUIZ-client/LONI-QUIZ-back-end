87ef22c027a238d228ae1688b6f857dd9edc7845
SeungHanLee <swq3064@naver.com>
2024-02-01 17:21:03 +0900

    #정말 모르겠다

--- src/main/java/com/loniquiz/comment/lobby/controller/ChatController.java
+++ src/main/java/com/loniquiz/comment/lobby/controller/ChatController.java
@@ -3,6 +3,7 @@ package com.loniquiz.comment.lobby.controller;
 import com.loniquiz.chatEntity.ChatResponse;
 import com.loniquiz.comment.lobby.dto.response.GameChatResponseDTO;
 import com.loniquiz.comment.lobby.dto.response.MemberResponseDTO;
+import com.loniquiz.comment.lobby.dto.response.RoomIsFullDTO;
 import com.loniquiz.game.members.service.MembersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -36,13 +37,13 @@ public class ChatController {
     @SendTo("/topic/game/messages")
     public GameChatResponseDTO gameSendMessage(@Payload GameChatResponseDTO res){
         res.setLocalDateTime(LocalDateTime.now());
-        System.out.println("res = " + res);
         return res;
     }
 
     @MessageMapping("/game/members")
     @SendTo("/topic/game/members")
-    public String gameSendMembers(@Payload MemberResponseDTO dto){
+    public RoomIsFullDTO gameSendMembers(@Payload MemberResponseDTO dto){
+        RoomIsFullDTO roomIsFullDTO = new RoomIsFullDTO();
 
         // 같은 gno를 가진 사용자 수를 세기 위한 변수
         int countSameGnoUsers = 0;
@@ -54,18 +55,21 @@ public class ChatController {
             }
             // 이미 아이디와 방 번호가 같은게 있으면 null 반환
             if(memberResponseDTO.getUserId().equals(dto.getUserId()) && memberResponseDTO.getGno().equals(dto.getGno())){
-                return "false";
+                roomIsFullDTO.setIsFull("false");
+                return roomIsFullDTO;
             }
         }
 
         // 같은 gno를 가진 사용자가 maxUser를 초과하는 경우 null 반환
         if(countSameGnoUsers >= dto.getMaxUser()) {
-            return "false";
+            roomIsFullDTO.setIsFull("true");
+            return roomIsFullDTO;
         }
 
         // 모든 조건을 통과한 경우에만 member 리스트에 추가
         member.add(dto);
-        return "true";
+        roomIsFullDTO.setIsFull("false");
+        return roomIsFullDTO;
 
     }
 
