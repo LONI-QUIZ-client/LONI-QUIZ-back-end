f6dd18b7dbde7a9617054e83b40e1c1916bf17d6
bumjun2 <jbj3713@naver.com>
2024-02-01 23:36:44 +0900

    홀호

--- src/main/java/com/loniquiz/comment/lobby/controller/ChatController.java
+++ src/main/java/com/loniquiz/comment/lobby/controller/ChatController.java
@@ -3,26 +3,31 @@ package com.loniquiz.comment.lobby.controller;
 import com.loniquiz.chatEntity.ChatResponse;
 import com.loniquiz.comment.lobby.dto.response.GameChatResponseDTO;
 import com.loniquiz.comment.lobby.dto.response.MemberResponseDTO;
+import com.loniquiz.game.members.dto.response.GameRoomResponseDTO;
 import com.loniquiz.game.members.service.MembersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller
-@RequiredArgsConstructor
 public class ChatController {
 
-    private final MembersService membersService;
     List<MemberResponseDTO> member = new ArrayList<>();
 
+    private final SimpMessagingTemplate messagingTemplate;
 
+    public ChatController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
 
 
     @MessageMapping("/chat")
@@ -75,6 +80,47 @@ public class ChatController {
     public List<MemberResponseDTO> gameSendMessage(@Payload String gno) throws InterruptedException {
         Thread.sleep(200);
         return member;
-
     }
+
+//    @MessageMapping("/game/timer")
+//    @SendTo("/topic/game/timer")
+//    public int sendTimer() {
+//        int i = startCountdown();
+//
+//        return i;
+//    }
+
+//    public int startCountdown() {
+//        Timer timer = new Timer(true);
+//
+//        timer.scheduleAtFixedRate(new TimerTask() {
+//            @Override
+//            public void run() {
+//                if (countdown > 0) {
+//                    System.out.println("남은 시간: " + countdown + "초");
+//                    countdown--;
+//                } else {
+//                    System.out.println("카운트다운 종료!");
+//                    timer.cancel(); // 타이머 종료
+//                }
+//            }
+//        }, 0, 1000); // 0초부터 시작해서 1초 간격으로 실행
+//        return countdown;
+//    }
+
+
+    @Scheduled(fixedRate = 1000) // 1초마다 메시지 전송
+    public void sendTimerUpdate() {
+        // 여기에서 실제 타이머 값 계산 및 전송
+        int timerValue = calculateTimerValue();
+        messagingTemplate.convertAndSend("/topic/game/timer", String.valueOf(timerValue));
+    }
+
+    private int calculateTimerValue() {
+        // 실제 타이머 값 계산 로직 추가
+        return 0;
+    }
+
+
+
 }
---
+++ src/main/java/com/loniquiz/game/members/dto/response/GameRoomResponseDTO.java
@@ -0,0 +1,14 @@
+package com.loniquiz.game.members.dto.response;
+
+import lombok.*;
+
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GameRoomResponseDTO {
+    private String timer;
+}
